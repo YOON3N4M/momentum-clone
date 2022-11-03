ee22d77846abfeca3715995a4177f3eed275dcc2
yoon3nam <lgmysn2@gmail.com>
2022-11-04 00:32:58 +0900

    디자인 틀 잡기전. 기능적인 면들 백업 css나 html이나 난잡한 상태 더 복잡해지기전에 일단 백업후 작업예정

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.png", "1.png", "2.png"]; // 이미지 목록 배열 생성
+const images = ["0.png", "1.png", "2.png", "3.jpg", "4.jpg", "5.jpg", "6.jpg"]; // 이미지 목록 배열 생성
 const chosenImage = images[Math.floor(Math.random() * images.length)]; // image[랜덤으로 생성되는 숫자]
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
